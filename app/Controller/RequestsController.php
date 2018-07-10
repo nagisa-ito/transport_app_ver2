6926cbb6cf777ea57dd5dbf11b1006c224c51a60
伊藤凪冴 <nagisa.ito@e-grant.net>
2018-07-10 18:31:51 +0900

    不要なファイルを削除

--- app/Controller/RequestsController.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    class RequestsController extends AppController {
-        public $scaffold;
-    }
--- app/webroot/js/detailsscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#myStat').circliful({
-        animationStep: 12,
-        percent: 100,
-        foregroundColor: '#1bab9e',
-        text: total_cost,
-        noPercentageSign: true,
-    });
-});
