8482e92388473943ec28ffcb162c77d040c68ef0
伊藤凪冴 <nagisa.ito@e-grant.net>
2018-07-11 11:09:40 +0900

    ページネーション対応

--- app/Controller/SectionsController.php
+++ app/Controller/SectionsController.php
@@ -4,6 +4,11 @@
     {
         public $helpers = array('Html', 'Form',);
         public $uses = array('RequestDetail', 'User', 'Department', 'Section');
+        public $components = array('Paginator');
+        public $paginate = array(
+            'limit' => 5,
+            'order' => array('Section.id' => 'ASC'),
+        );
         
         public function beforeFilter()
         {
@@ -16,6 +21,10 @@
         {
             $sections = $this->Section->find('all');
             $this->set(compact('sections'));
+            
+            $this->Paginator->settings = $this->paginate;
+            $data = $this->Paginator->paginate('Section');
+            $this->set(compact('data'));
         }
         
         public function admin_index()
