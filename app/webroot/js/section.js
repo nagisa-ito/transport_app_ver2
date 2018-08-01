d767c0a8c14c00e75399b12e3333a497fb28c323
伊藤凪冴 <nagisa.ito@e-grant.net>
2018-08-01 11:35:00 +0900

    区間マスタの削除が動くように変更

--- app/webroot/js/section.js
+++ app/webroot/js/section.js
@@ -2,7 +2,7 @@ $(function() {
     $('a.delete_icon').click(function(e) {
         console.log($(this).data('id'));
         if(confirm('削除しますか？')) {
-            $.post('/transport_expense_app/sections/delete/' + $(this).data('id'), {},
+            $.post('/transportation_expenses_app_v2/sections/delete/' + $(this).data('id'), {},
                 function(response) {
                     $('#section_'+response.id).fadeOut();
                 }, "json");
